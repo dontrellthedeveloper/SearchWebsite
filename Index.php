30a3b68302fcd548208fa409d537ce84d1eee6a7
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-02 22:55:04 -0700

    Initial Commit

---
+++ Index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Welcome to Tunel Search</title>
+</head>
+<body>
+
+
+    <div class="mainSection">
+
+        <div class="logoContainer">
+            
+            <img src="img/TunelLogo2.jpg" alt="">
+            
+        </div>
+    
+        <div class="searchContainer">
+
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" name="Search">
+
+        </div>    
+            
+        
+    </div>
+
+
+</body>
+</html>
