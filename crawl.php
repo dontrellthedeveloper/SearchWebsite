478a8ff55b96c8e383b1732cb563498f24394327
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-04 12:50:52 -0800

    Ignoring links I don't want to use

--- crawl.php
+++ crawl.php
@@ -1,6 +1,13 @@
 <?php
 include("classes/DomDocumentParser.php");
 
+
+function createLink($src, $url) {
+    echo "SRC: $src<br>";
+    echo "URL: $url<br>";
+}
+
+
 function followLinks($url) {
 
     $parser = new DomDocumentParser($url);
@@ -8,6 +15,16 @@ function followLinks($url) {
 
     foreach ($linkList as $link) {
         $href = $link->getAttribute("href");
+
+        if(strpos($href, "#") != false) {
+            continue;
+        }
+        else if (substr($href, 0, 11) == "javascript:") {
+            continue;
+        }
+
+        createLink($href, $url);
+
         echo $href . "<br>";
     }
 
