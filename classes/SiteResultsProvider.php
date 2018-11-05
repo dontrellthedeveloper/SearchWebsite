a56f77aef33124cb697f19538d774f8925f8d9f9
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-04 20:40:36 -0800

    Trim the Results Text

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -36,6 +36,9 @@
                 $title = $row["title"];
                 $description = $row["description"];
 
+                $title = $this->trimField($title, 55);
+                $description = $this->trimField($description, 230);
+
                 $resultsHtml .= "<div class='resultContainer'>
 
 								<h3 class='title'>
@@ -54,6 +57,12 @@
             $resultsHtml .= "</div>";
 
             return $resultsHtml;
+        }
+
+        private function trimField($string, $characterLimit) {
+
+            $dots = strlen($string) > $characterLimit ? "..." : "";
+            return substr($string, 0, $characterLimit) . $dots;
         }
 
     }
