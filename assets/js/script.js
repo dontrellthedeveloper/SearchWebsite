3da56ddfad5c4fe586f0a524f46c7337d3c03f8f
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-04 23:46:38 -0800

    Getting the id of the clicked links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
 
     $(".result").on("click", function() {
 
+        var id = $(this).attr("data-linkId");
         var url = $(this).attr("href");
-        console.log(url)
+
+        if(!id) {
+            alert("data-linkId attribute not found");
+        }
+
+        increaseLinkClicks(id, url);
 
        return false;
     });
 
 });
 
-function increaseLinkClicked(linkedId, url,) {
+function increaseLinkClicks(linkId, url,) {
     
 }
