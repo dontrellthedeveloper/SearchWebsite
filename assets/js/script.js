d423e451c3af12366378f2f5d67680abb59afe8c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-11-05 23:10:26 -0800

    Update the clicks value via Ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,15 @@ $(document).ready(function() {
 
 });
 
-function increaseLinkClicks(linkId, url,) {
-    
+function increaseLinkClicks(linkId, url) {
+
+    $.post("ajax/updateLinkCount.php", {linkId: linkId})
+        .done(function(result) {
+            if(result != "") {
+                alert(result);
+                return;
+            }
+
+            window.location.href = url;
+        });
 }
